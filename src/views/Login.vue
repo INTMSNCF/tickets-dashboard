a233999a0dde3cc198bfe09ddc111c58eeec19ea
Moobster <rhea.moubarak@gmail.com>
2020-12-29 09:48:22 +0100

    Modal de RDP Fixes #1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -24,22 +24,44 @@
                   <v-text-field
                     v-model="user.password"
                     prepend-icon="mdi-lock"
-                    label="Password"
+                    label="Mot de passe"
                     type="password"
                   ></v-text-field>
-                  <v-checkbox
+          
+                 <v-checkbox
                     v-if="options.isLoggingIn"
-                    v-model="options.shouldStayLoggedIn"
+                    v-model="checkbox"
                     light
-                    label="Reste connecte ?"
-                  ></v-checkbox>
-                  <v-btn
+                  >
+                    <template slot="label">
+                  Rester connecté
+                  <v-dialog v-model="dialog" persistent max-width="290">
+                    
+                  <v-icon class="mx-1" style="margin-top: -1em;" x-small color="blue darken-1" @click.prevent="dialog = true" slot="activator">mdi-information-outline</v-icon>
+                  <v-card>
+                  <v-card-title class="headline">Termes et conditions</v-card-title>
+                  <v-card-text>Dans le cadre de la RGPD Règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016, relatif à 
+                    la protection des personnes physiques à l'égard du traitement des données à caractère personnel et à la libre circulation de ces données, 
+                    et abrogeant la directive 95/46/CE (règlement général sur la protection des données), nous vous informons que seule l’adresse mail et le mot 
+                    de passe (mot de passe FRESHDESK) sont collectés et stockés sur le poste de travail</v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialog = false; checkbox=false;">Désaccord</v-btn>
+                    <v-btn color="blue darken-1" text @click="dialog = false; checkbox=true;">Accord</v-btn>
+                  </v-card-actions>
+                   </v-card>
+                  </v-dialog>
+                  </template>
+                  </v-checkbox>
+                  
+                  
+                    <v-btn
                     v-if="options.isLoggingIn"
                     @click.prevent=""
                     block="block"
                     type="submit"
-                    >Se connecte</v-btn
-                  >
+                    >Se connecter
+                    </v-btn>
                 </v-form>
               </v-card-text>
             </v-card>
@@ -71,6 +93,8 @@ export default {
     thisYear: dayjs().format("YYYY"),
     version: document.body.getAttribute("version"),
     build: document.body.getAttribute("build"),
+    dialog: false,
+    checkbox: true,
   }),
 };
 </script>
